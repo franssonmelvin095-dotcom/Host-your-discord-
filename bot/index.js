83a559a9906c5370a04590114e756b8112fb85b3
franssonmelvin095-dotcom <franssonmelvin095@gmail.com>
2026-03-11 18:32:22 +0100

    Add Discord bot main file

---
+++ bot/index.js
@@ -0,0 +1,9 @@
+// Discord bot main file
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(process.env.BOT_TOKEN);
